ff664fa906c62a5f1d2eb836e9587568ec283746
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2022-12-06 20:55:51 +0530

    added logic for total

--- main.js
+++ main.js
@@ -33,38 +33,35 @@ const shoppingIcon = document.querySelectorAll(".shopping__icon");
 
 // added the onclick for add to  cart
 
-window.addEventListener();
-
-shoppingIcon.forEach((icon) => {
-  icon.addEventListener("click", (e) => {
-    if (e.target.classList.contains("shopping__icon")) {
-      sideBar.classList.add("show__sidebar");
-      addToCart(e.target);
-    }
+window.addEventListener("DOMContentLoaded", () => {
+  shoppingIcon.forEach((icon) => {
+    icon.addEventListener("click", (e) => {
+      if (e.target.classList.contains("shopping__icon")) {
+        sideBar.classList.add("show__sidebar");
+        addToCart(e.target);
+      }
+    });
   });
-});
 
-function addToCart(target) {
-  //   console.log(
-  //     target.parentElement.parentElement.querySelector(".product__main__image")
-  //       .src
-  //   );
+  function addToCart(target) {
+    // console.log(target.parentElement.parentElement);
 
-  const price = target.parentElement
-    .querySelector(".product__price")
-    .innerHTML.split(" ")[1];
-  const name =
-    target.parentElement.parentElement.querySelector(
-      ".product__name"
-    ).innerHTML;
-  const src = target.parentElement.parentElement.querySelector(
-    ".product__main__image"
-  ).src;
+    const price = target.parentElement
+      .querySelector(".product__price")
+      .innerHTML.split(" ")[1];
+    const name =
+      target.parentElement.parentElement.querySelector(
+        ".product__name"
+      ).innerHTML;
 
-  const container = document.createElement("article");
-  container.className = "cart__card";
-  //   console.log(container);
-  container.innerHTML = ` 
+    const src = target.parentElement.parentElement.querySelector(
+      ".product__main__image"
+    ).src;
+
+    const container = document.createElement("article");
+    container.className = "cart__card";
+    //   console.log(container);
+    container.innerHTML = ` 
               <img
                 src="${src}"
                 alt="product 1"
@@ -78,12 +75,37 @@ function addToCart(target) {
                   name="number"
                   id="number"
                   class="cart__number"
+                  value="1"
                 />
               </div>
               <button class="btn btn--delete">
                 <i class="fa-solid fa-trash"></i>
               </button>`;
 
-  const containerCart = getElement(".cart__container");
-  containerCart.appendChild(container);
+    const containerCart = getElement(".cart__container");
+    containerCart.appendChild(container);
+
+    containerCart.addEventListener("click", (e) => {
+      console.log(e.target.parentElement);
+      const cardElement = e.target.parentElement.parentElement;
+
+      const quantity =
+        e.target.parentElement.querySelector(".cart__number").value;
+      let total = 0;
+
+      // console.log(quantity);
+      if (e.target.parentElement.classList.contains("btn--delete")) {
+        cardElement.remove();
+      }
+      totalAmount(price, quantity, total);
+    });
+  }
+});
+
+function totalAmount(price, quantity, total) {
+  total += price * quantity;
+
+  const totalPrice = document.querySelector(".total");
+
+  totalPrice.innerHTML = total;
 }
